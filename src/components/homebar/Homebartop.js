c5ca61dee0ea23566dd933ce931f346b59a08613
md-nahid <nahid98learn@gmail.com>
2021-06-25 21:40:09 +0600

    added user img in the home bar section

--- src/components/homebar/Homebartop.js
+++ src/components/homebar/Homebartop.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Homebartop.scss"
 import Staricon from "../../images/staricon.jpg"
+import Userimg from "../../images/userphoto.png"
 
 
 
@@ -10,7 +11,10 @@ const Homebartop = () => {
     return (
         <div className="homebartopcontainer">
             <div>
-                <h3>Home</h3>
+                <div>
+                    <img src={Userimg} alt="This is user." />
+                    <h3>Home</h3>
+                </div>
                 <img src={Staricon} alt="twitter star icon" />
             </div>
         </div>
